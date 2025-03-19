fb888a107040cbbbfccd7753afda70bd8b2b2403
palakSingh621 <kushwahps621@gmail.com>
2025-03-19 14:31:53 +0530

    [Palak] Add: UC3 Get work Hours

--- Employee.js
+++ Employee.js
@@ -29,3 +29,19 @@ switch (empCheck){
 }
 let empWage= empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
+//UC3
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+empHrs=0;
+empCheck = Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(empCheck);
+empWage = empHrs* WAGE_PER_HOUR;
+console.log("Emp Wage: "+empWage);
