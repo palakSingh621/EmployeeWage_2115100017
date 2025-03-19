b08c7e24d48c1c55fc04a8987a574451ed72b478
palakSingh621 <kushwahps621@gmail.com>
2025-03-19 15:12:41 +0530

    [Palak] Add: UC6 Storing daily Wage in Array

--- Employee.js
+++ Employee.js
@@ -68,3 +68,23 @@ while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORK_DAYS)
 }
 empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("UC5 - Total Days: "+totalWorkingDays+ "Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+//UC6
+function calcDailyWage(empHrs)
+{
+    return empHrs*WAGE_PER_HOUR;
+}
+const HRS_IN_MONTH= 160;
+const WORKING_DAYS= 20;
+totalEmpHrs =0;
+totalWorkingDays =0;
+let empDailyWageArr= new Array();
+while(totalEmpHrs <=HRS_IN_MONTH && totalWorkingDays < WORKING_DAYS)
+{
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6-Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+ " Emp Wage: "+empWage);
