d0f51bfdd3dde711ce1ae66386de0492da38ed2c
palakSingh621 <kushwahps621@gmail.com>
2025-03-19 15:00:40 +0530

    [Palak] Add: UC5 Wages till number of working days

--- Employee.js
+++ Employee.js
@@ -55,3 +55,16 @@ for(let day=0; day < NUM_OF_WORKING_DAYS; day++)
 }
 empWage = empHrs* WAGE_PER_HOUR;
 console.log("Total Hrs: "+empHrs+ " Emp Wage: "+empWage);
+//UC5
+const MAX_HRS_IN_MONTH =100;
+const NUM_OF_WORK_DAYS = 10;
+let totalEmpHrs=0;
+let totalWorkingDays =0;
+while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORK_DAYS)
+{
+    totalWorkingDays++;
+    let empCheck =Math.floor(Math.random()*10)% 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 - Total Days: "+totalWorkingDays+ "Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
