802d6623005f58be155bc5119afb73610cdb1d14
palakSingh621 <kushwahps621@gmail.com>
2025-03-19 14:38:53 +0530

    [Palak] Add: UC4 Wages for a month

--- Employee.js
+++ Employee.js
@@ -45,3 +45,13 @@ empCheck = Math.floor(Math.random()*10)%3;
 empHrs=getWorkingHours(empCheck);
 empWage = empHrs* WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
+//UC4
+const NUM_OF_WORKING_DAYS=2;
+empHrs=0;
+for(let day=0; day < NUM_OF_WORKING_DAYS; day++)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
+}
+empWage = empHrs* WAGE_PER_HOUR;
+console.log("Total Hrs: "+empHrs+ " Emp Wage: "+empWage);
