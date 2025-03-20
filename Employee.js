661c494570cd7e0c36ca6774e45cd8da03c49c7d
palakSingh621 <kushwahps621@gmail.com>
2025-03-20 15:47:10 +0530

    [Palak] Add: UC10 Object Creation

--- Employee.js
+++ Employee.js
@@ -160,3 +160,24 @@ empDailyHrsMap.forEach((value, key, map)=> {
 console.log("Full Working Days: "+fullWorkingDays);
 console.log("Part Working Days: "+partWorkingDays);
 console.log("Non Working Days: "+nonWorkingDays);
+//UC10
+totalEmpHrs =0;
+totalWorkingDays = 0;
+let empDailyHrsAndWageArr = new Array();
+while (totalEmpHrs <= HRS_IN_MONTH && totalWorkingDays <WORKING_DAYS){
+         totalWorkingDays++;
+         let empCheck = Math.floor(Math.random() * 10)%3;
+         let empHrs = getWorkingHours(empCheck);
+         totalEmpHrs += empHrs;
+         empDailyHrsAndWageArr.push(
+            {
+               dayNum:totalWorkingDays,
+               dailyHours:empHrs,
+               dailyWage: calcDailyWage(empHrs),
+               toString(){
+                  return '\nDay'+ this.dayNum + '=> Working Hours is' + this.dailyHours +'And Wage Earned = ' + this.dailyWage
+               },
+
+            });
+      }
+      console.log("UC 10 Showing Daily Hours Worked and Wage Earned: "+empDailyHrsAndWageArr);
